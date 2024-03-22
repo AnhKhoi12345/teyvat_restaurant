6cdb75b8563aba99614024bd77f532b94d0f190f
khoi <hnak2442001smg4@gmail.com>
2024-03-22 21:31:54 +0700

    Finished Navigation bar and Header, added welcome dection to begin Home's body

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -6,6 +6,8 @@ import { Image } from 'primereact/image';
 
 function Navbar() {
   const navlogo = useRef(null);
+  const navbar = useRef(null);
+  const menuBtn = useRef(null);
   const navList = [
     { id: 1, name: 'HOME', link: '/' },
     { id: 2, name: 'ABOUT', link: '/about' },
@@ -19,15 +21,42 @@ function Navbar() {
     { id: 2, icon: 'fa-brands fa-x-twitter', link: '/' },
     { id: 3, icon: 'fa-brands fa-youtube', link: '/' },
   ];
-
+  // eslint-disable-next-line no-array-constructor
+  const navListArray = useRef(new Array());
+  const navIconArray = useRef(new Array());
+  const navbarScroll = () => {
+    let scrollTop = window.pageYOffset;
+    if (scrollTop >= 10) {
+      navbar.current.style.backgroundColor = 'rgb(255, 255, 255,0.8)';
+      navlogo.current.style.background = 'url("https://i.imgur.com/3aE4bds.png") top no-repeat';
+      navlogo.current.style.backgroundSize = 'contain';
+      menuBtn.current.style.color = 'black';
+      for (let i = 0; i < navList.length; i++) {
+        navListArray.current[i].style.color = 'black';
+      }
+      for (let i = 0; i < navLogoList.length; i++) {
+        navIconArray.current[i].style.color = 'black';
+      }
+    } else {
+      for (let i = 0; i < navList.length; i++) {
+        navListArray.current[i].style.color = 'white';
+      }
+      for (let i = 0; i < navLogoList.length; i++) {
+        navIconArray.current[i].style.color = 'white';
+      }
+      navlogo.current.style.background = navbar.current.style.backgroundColor = '';
+      menuBtn.current.style.color = '';
+    }
+  };
+  window.addEventListener('scroll', navbarScroll);
   return (
-    <nav className="navbar-container">
+    <nav className="navbar-container" ref={navbar}>
       <Link className="navLogo" ref={navlogo} to="/"></Link>
       <ul className="navList">
         {navList.map((item) => {
           return (
             <li className="navList-item" key={item.id}>
-              <Link to={item.link} className="navList-item-text">
+              <Link ref={(element) => navListArray.current.push(element)} to={item.link} className="navList-item-text">
                 {item.name}
               </Link>
             </li>
@@ -39,13 +68,16 @@ function Navbar() {
           return (
             <li className="navLogo-item" key={item.id}>
               <Link to={item.link}>
-                <i className={item.icon}></i>
+                <i ref={(element) => navIconArray.current.push(element)} className={item.icon}></i>
               </Link>
             </li>
           );
         })}
       </ul>
-      <Button icon="fa-solid fa-bars" rounded text aria-label="Filter" size="large"></Button>
+      <Button text aria-label="Filter" size="large">
+        {' '}
+        <i ref={menuBtn} className="fa-solid fa-bars"></i>
+      </Button>
     </nav>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,27 @@
 import './home.scss';
-
+import charlotteSpecialty from '../../images/ExclusiveScoopGourmetColumn1.webp';
+import { Link } from 'react-router-dom';
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <section className="body-welcome-container">
+        <div className="body-welcome-text">
+          <h2 className="body-restaurant-name">Teyvatian Restaurant</h2>
+          <h1 className="body-welcome">WELCOME</h1>
+          <p className="body-weldome-p">
+            Welcome Travelers to the official restaurant run by Hoyofood, here travelers can enjoy many wonderful
+            Teyvatian dishes from many nations from Genshin Impact
+          </p>
+          <Link className="our-story" to="/menu">
+            <b>
+              OUR STORY <i class="fa-solid fa-arrow-right"></i>
+            </b>
+          </Link>
+        </div>
+        <div className="body-welcome-image-container">
+          <img className="body-welcome-image" src={charlotteSpecialty} alt="Charlottes Specialty" />
+        </div>
+      </section>
     </div>
   );
 }
